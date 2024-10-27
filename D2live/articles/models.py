5a8bceadf6e72324d89abdda85b20151a93151e6
Junho Seo <sjh2395@naver.com>
2024-10-28 08:19:10 +0900

    241028 morning

--- D2live/articles/models.py
+++ D2live/articles/models.py
@@ -14,7 +14,8 @@ class Comment(models.Model):
     # 외래 키의 ForeignKey 클래스 작성 위치에 관계 없이 테이블에서는 맨 뒤쪽에 생성됨
     # 필드 명은 상대 모델 클래스명의 단수형으로 작성
     # 데이터 무결성 원칙을 지키기 위한 두 가지 방법
-    #   - 게시글을 삭제했을 때 달려있는 댓글도 모두 삭제(실습)
+    #   - 게시글을 삭제했을 때 달려있는 댓글
+    # .도 모두 삭제(on_delete의 CASCADE)
     #   - 댓글이 달려있는 게시글은 삭제할 수 없도록 처리
     # article = models.ForeignKey(상대 모델 클래스, 상대 모델 클래스가 삭제되었을 때 어떻게 처리할지)
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
